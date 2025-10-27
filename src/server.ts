6612d0b737396649589ee91d69ec1c14d5b756df
Berlin Ortega <bortegal1700@alumno.ipn.mx>
2025-10-26 19:11:36 -0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,6 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
-//
 app.use("/api/sensorTypeConfig", sensorTypeConfigRoutes);
 app.use("/api/esp32Type", esp32TypeRoutes);
 app.use("/api/records", recordsRoutes);
